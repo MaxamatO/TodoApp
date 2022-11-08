3c84385e2b0e6b2d3ed0e7dfe71f83e77c90a5af
Maksymilian Pierchała <maksdfg123@gmail.com>
2022-11-08 21:54:34 +0100

    Add popup window - to complete

---
+++ src/TaskPopup.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./TaskPopup.css";
+import { PlusSquareFill, XSquareFill } from "react-bootstrap-icons";
+import { motion } from "framer-motion";
+
+// TODO: Complete the Popupwindow layout
+function TaskPopup(props) {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                
+                <h3>Task details press to close</h3>
+                <button onClick={(()=>{props.setTrigger(false)})}>X</button>
+                <div className="content">
+                    <p className="task-info">{props.children}</p>
+                </div>
+                <div className="popup-footer">
+                    <p className="date-info">Created at: {props.date}</p>
+                    <motion.button
+                    whileTap={{ scale: 1 }}
+                    whileHover={{ scale: 1.05 }}
+                    className="popup-btn"
+                    >
+                    <XSquareFill className="popup-xbtn" />
+                </motion.button>
+                </div>
+            </div>
+        </div>
+    ) : "";
+}
+
+export default TaskPopup;
